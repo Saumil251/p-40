47919256b5816da63e195ead5ae71588f476c758
Saumil251 <72085873+Saumil251@users.noreply.github.com>
2021-02-02 21:27:58 +0530

    Add files via upload

--- Game.js
+++ Game.js
@@ -1,96 +1,75 @@
-class Game {
-  constructor(){
+class Game{
+    constructor(){
 
-  }
+    }
+    getState() {
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value", function (data) {
+            gameState = data.val();
+        })
 
-  getState(){
-    var gameStateRef  = database.ref('gameState');
-    gameStateRef.on("value",function(data){
-       gameState = data.val();
-    })
-
-  }
-
-  update(state){
-    database.ref('/').update({
-      gameState: state
-    });
-  }
-
-  async start(){
-    if(gameState === 0){
-      player = new Player();
-      var playerCountRef = await database.ref('playerCount').once("value");
-      if(playerCountRef.exists()){
-        playerCount = playerCountRef.val();
-        player.getCount();
-      }
-      form = new Form()
-      form.display();
     }
 
-    car1 = createSprite(100,200);
-    car2 = createSprite(300,200);
-    car3 = createSprite(500,200);
-    car4 = createSprite(700,200);
-    cars = [car1, car2, car3, car4];
-  }
-
-  play(){
-    form.hide();
+    update(state) {
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+    async start() {
+            if (gameState === 0) {
+                player = new Player();
+                var playerCountRef = await database.ref('playerCount').once("value");
+                if (playerCountRef.exists()) {
+                    playerCount = playerCountRef.val();
+                    player.getCount();
+                }
+                form = new Form()
+                form.display();
+            }
+    player1 = createSprite(200,500);
+    player1.addImage("player1",player_img);
     
-    Player.getPlayerInfo();
-    
-    if(allPlayers !== undefined){
-      
-      
-      //var display_position = 100;
-      
-      //index of the array
-      var index = 0;
+    player2 = createSprite(800,500);
+    player2.addImage("player2", player_img);
+    players=[player1,player2];
 
-      //x and y position of the cars
-      var x ;
-      var y=175
-
-      for(var plr in allPlayers){
-        //add 1 to the index for every loop
-        index = index + 1 ;
-
-        //position the cars a little away from each other in x direction
-        y = y + 200;
-        //use data form the database to display the cars in y direction
-        x = displayWidth - allPlayers[plr].distance;
-        cars[index-1].x = x;
-        cars[index-1].y = y;
-
-        if (index === player.index){
-         
-          cars[index - 1].shapeColor = "red";
-          camera.position.x = cars[index-1].x;
-          camera.position.y = displayHeight/2
         }
-       
-        //textSize(15);
-        //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
-      }
+    
+    play(){
+        
+        form.hide();
 
+        Player.getPlayerInfo();
+        image(back_img, 0, 0, 1000, 800);
+        var x =100;
+        var y=200;
+        var index =0;
+        drawSprites();
+
+        for(var plr in allPlayers){
+        
+            index = index+1;
+            x = 500-allPlayers[plr].distance;
+            y=500;
+            
+            players[index -1].x = x;
+            players[index - 1].y = y;
+
+            // Differentiate the main player by printing
+            // the name of the player on the basket. 
+
+        }
+
+
+        // Give movements for the players using arrow keys
+
+
+        // Create and spawn fruits randomly
+
+        
     }
 
-    if(keyIsDown(UP_ARROW) && player.index !== null){
-      player.distance +=10
-      player.update();
+    end(){
+       console.log("Game Ended");
     }
-
-    if(player.distance > 3860){
-      gameState = 2;
-    }
-   
-    drawSprites();
-  }
-
-  end(){
-    console.log("Game Ended");
-  }
 }
-
--- sketch.js
+++ sketch.js
@@ -1,28 +1,41 @@
-var canvas, backgroundImage;
-
-var gameState = 0;
-var playerCount;
-var allPlayers;
-var distance = 0;
 var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
+var score =0;
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
+var fruitGroup;
+var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
+var player_img;
+var player1score =0;
+var player2score =0;
 
-var form, player, game;
-var cars,car1,car2,car3,car4
-function setup(){
-  canvas = createCanvas(displayWidth-20,displayHeight-30);
+function preload(){
+  back_img = loadImage("images/jungle.jpg");
+  player_img = loadImage("images/basket2.png");
+  fruit1_img = loadImage("images/apple2.png");
+  fruit2_img = loadImage("images/banana2.png");
+  fruit3_img = loadImage("images/melon2.png");
+  fruit4_img = loadImage("images/orange2.png");
+  fruit5_img = loadImage("images/pineapple2.png");
+  fruitGroup = new Group();
+}
+function setup() {
+  createCanvas(1000, 600);
   database = firebase.database();
   game = new Game();
   game.getState();
   game.start();
+  
 }
 
+function draw() {
+  background(back_img);
 
-function draw(){
-  if(playerCount === 4){
-    game.update(1);
-  }
-  if(gameState === 1){
-    clear();
-    game.play();
-  }
+  // Add conditions for gameStates and playerCount
+
 }
